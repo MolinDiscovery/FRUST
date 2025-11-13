d1127cd66a76d0a5390c5a585f4cada72527b05a
MolinDiscovery <jacob.molin@me.com>
2025-11-13 22:12:43 +0100

    clean up

--- scripts/submit2_split2.py
+++
@@ -1,8 +0,0 @@
-import importlib
-
-mod = importlib.import_module("frust.pipes.run_ts_per_rpos")
-steps = [
-    obj for name, obj in mod.__dict__.items()
-    if name.startswith("run_") and callable(obj)
-]
-
